bf7b7d773845c4e36a260ac81331446adc860baa
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-13 19:24:14 +0530

    Function & Parameter

--- demo.cpp
+++ demo.cpp
@@ -1,138 +1,20 @@
 #include <iostream>
 using namespace std;
+
+int minNum(int a, int b)
+{
+    if (a < b)
+    {
+        return b;
+    }
+    else
+    {
+        return a;
+    }
+}
+
 int main()
 {
-    int n = 4;
-    // Patterns
-
-    char ch = 'A';
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n; j++)
-        {
-            cout << ch << ' ';
-            ch++;
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    int a = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < a; j++)
-        {
-            cout << "* ";
-        }
-        a++;
-        cout << endl;
-    }
-
-    // ------ //
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 1; j <= i + 1; j++)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = i + 1; j > 0; j--)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    int num = 1;
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << num << " ";
-            num++;
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << " ";
-        }
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << (i + 1) << " ";
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    for (int i = 0; i <= n; i++)
-    {
-        for (int j = 0; j < n - i; j++)
-        {
-            cout << " ";
-        }
-        for (int j = 1; j < i; j++)
-        {
-            cout << j;
-        }
-        for (int j = i; j > 0; j--)
-        {
-            cout << j;
-        }
-        cout << endl;
-    }
-
-    // ------ //
-
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            cout << " ";
-        }
-        cout << "*";
-        if (i != 0)
-        {
-            for (int j = 0; j < 2 * i - 1; j++)
-            {
-                cout << " ";
-            }
-            cout << "*";
-        }
-        cout << endl;
-    }
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < i + 1; j++)
-        {
-            cout << " ";
-        }
-        cout << "*";
-        if (i != n - 2)
-        {
-            for (int j = 0; j < 2 * (n - i) - 5; j++)
-            {
-                cout << " ";
-            }
-            cout << "*";
-        }
-        cout << endl;
-    }
+    cout << minNum(5, 8);
     return 0;
 }
