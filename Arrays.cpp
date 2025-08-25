467b73cf3e15b374c31efbffce5423aa64a653ff
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-25 19:07:24 +0530

    Arrays

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
+#include <climits>
 using namespace std;
+
+// int LSA(int num[], int size, int target)
+// {
+//     for (int i = 0; i < size; i++)
+//     {
+//         if (target == num[i])
+//         {
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
 
 int main()
 {
     // int numbers[5] = {1, 2, 3, 4, 5}; // Arrays
     // int num[10];
-
     // int size = sizeof(num) / sizeof(int); // Loops in Arrays
     // for (int i = 0; i < size; i++)
     // {
@@ -17,15 +29,45 @@ int main()
     // }
 
     // Find smallest/largest in Array
-    int num[5] = {88, 18, 73, 53, 85};
-    int size = sizeof(num) / sizeof(int);
+    // int num[5] = {88, 18, 73, 53, 85};       //Smallest
+    // int size = sizeof(num) / sizeof(int);
+    // int small = INT_MAX;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if (num[i] < small)
+    //         small = num[i];
+    // }
+    // cout << small << endl;
+    //////////////////////////////////////////
+    // int num[5] = {88, 18, 73, 53, 85}; // Largest
+    // int size = sizeof(num) / sizeof(int);
+    // int big = INT_MIN;
+    // for (int i = 0; i < size; i++)
+    // {
+    //     if (num[i] < big)
+    //         big = num[i];
+    // }
+    // cout << big << endl;
 
-    int small = INT8_MAX;
+    // Linear Search
+    // int num[5] = {83, 22, 44, 75, 23};
+    // int size = sizeof(num) / sizeof(int);
+    // int target = 84;
+    // cout << LSA(num, size, target) << endl;
+
+    int num[7] = {3, 6, 4, 5, 2, 1, 5};
+    int size = sizeof(num) / sizeof(int);
+    int start = 0, end = size - 1;
+    while (start < end)
+    {
+        swap(num[start], num[end]);
+        start++;
+        end--;
+    }
     for (int i = 0; i < size; i++)
     {
-        if (num[i] < small)
-            small = num[i];
+        cout << num[i];
     }
-    cout << small << endl;
+    cout << endl;
     return 0;
 }
