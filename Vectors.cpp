8f21fb8c9771d91f7a3f224df9ebd59b088d4e99
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-27 16:24:05 +0530

    Vectors

--- Vectors.cpp
+++ Vectors.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int main()
 {
-    // vector<int> vec; // 0
-    // vector<int> vecA = {1, 2, 3};
-    // vector<int> vecB(3, 0);
-    // cout << vecB[0] << endl;
-    // cout << vecB[1] << endl;
-    // cout << vecB[2] << endl;
+    vector<int> vec; // 0
+    vector<int> vecA = {1, 2, 3};
+    vector<int> vecB(3, 0);
+    cout << vecB[0] << endl;
+    cout << vecB[1] << endl;
+    cout << vecB[2] << endl;
 
     // for each loop
-    // vector<char> vecC = {'a', 'b', 'c', 'd'};
-    // for (char val : vecC)
-    // {
-    //     cout << val << endl;
-    // }
+    vector<char> vecC = {'a', 'b', 'c', 'd'};
+    for (char val : vecC)
+    {
+        cout << val << endl;
+    }
 
     // Vector Functions
-    // vector<char> vecD = {'a', 'b', 'c', 'd'};
-    // cout << "size = " << vecD.size() << endl; // size of the vector
-    // vecD.push_back('e');                      // enter a value at end
-    // vecD.pop_back();                          // delete the last value
-    // cout << vecD.front() << endl;             // show the first value
-    // cout << vecD.back() << endl;              // show the last value
-    // cout << vecD.at(2) << endl;               // access a value
+    vector<char> vecD = {'a', 'b', 'c', 'd'};
+    cout << "size = " << vecD.size() << endl; // size of the vector
+    vecD.push_back('e');                      // enter a value at end
+    vecD.pop_back();                          // delete the last value
+    cout << vecD.front() << endl;             // show the first value
+    cout << vecD.back() << endl;              // show the last value
+    cout << vecD.at(2) << endl;               // access a value
 
     // Size and Capacity
     vector<int> vecE;
@@ -34,5 +34,15 @@ int main()
     vecE.push_back(3); // It will double the capacity of vector if there is not size available
     cout << vecE.size() << endl;
     cout << vecE.capacity() << endl;
+
+    // Problem: 136. Single Number
+    vector<int> nums = {4, 1, 2, 1, 2};
+    // int singleNumber(vector<int> nums)
+    int ans = 0;
+    for (int val : nums)
+    {
+        ans = ans ^ val; // ans ^= val;
+    }
+    cout << ans << endl; // return ans;
     return 0;
 }
