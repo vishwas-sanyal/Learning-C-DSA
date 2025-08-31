b58e1880e503eb7bc159a11722f2987c9c33ccad
vishwas-sanyal <vishwassanyal2006@gmail.com>
2025-08-31 21:19:43 +0530

    Pow(x,n)

--- demo.cpp
+++ demo.cpp
@@ -25,10 +25,42 @@ int binToDecimal(int binNum)
     }
     return ans;
 }
+double myPow(double x, int n) // 50. Pow(x, n)
+{
+    // Corner Cases
+    //  if (n == 0)
+    //      return 1.0;
+    //  if (x == 0)
+    //      return 0.0;
+    //  if (x == 1)
+    //      return 1.0;
+    //  if (x = -1 && n % 2 == 0)
+    //      return 1.0;
+    //  if (x = -1 && n % 2 != 0)
+    //      return -1.0;
 
+    long binForm = n;
+    if (n < 0)
+    {
+        x = 1 / x;
+        binForm = -binForm;
+    }
+    double ans = 1;
+    while (binForm > 0)
+    {
+        if (binForm % 2 == 1)
+        {
+            ans *= x;
+        }
+        x *= x;
+        binForm /= 2;
+    }
+    return ans;
+}
 int main()
 {
     cout << decToBinary(50) << endl;
     cout << binToDecimal(110010) << endl;
+    cout << myPow(2.10000, 3) << endl;
     return 0;
 }
